a50ee8c3e076201984b9127a3331f8732a7c7080
Hernan Lozano <hernantz@gmail.com>
2013-04-21 12:21:21 -0300

    Added flask app config

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+
+# grabs the folder where the script runs
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+# secret key for session management and csrf
+SECRET_KEY = 'lotr43vA'
