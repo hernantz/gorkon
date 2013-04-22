90222d087d25b6ed750c9920abe1829cb9af21d0
Hernan Lozano <hernantz@gmail.com>
2013-04-22 09:40:02 -0300

    Set flask's DEBUG var in config.py

--- config.py
+++ config.py
@@ -6,3 +6,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 # secret key for session management and csrf
 SECRET_KEY = 'lotr43vA'
+
+DEBUG = True
+
+# X-Sendfile feature for files sent with send_file()
+USE_X_SENDFILE = True
