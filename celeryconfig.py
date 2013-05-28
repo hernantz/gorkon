0e9bdeb134bcd2cce728da2fc11b73923a4c44ff
Hernan Lozano <hernantz@gmail.com>
2013-05-28 16:13:19 -0300

    Removed duplicated and deprecated celery config vars

--- celeryconfig.py
+++ celeryconfig.py
@@ -1,5 +1,3 @@
-BROKER_URL = 'redis://localhost:6379/0'
-CELERY_RESULT_BACKEND = 'redis'
-CELERY_RESULT_BACKEND = 'redis://localhost:6379/0'
-CELERY_REDIS_PORT = 6379
+BROKER_URL = 'redis://localhost:6380/0'
+CELERY_RESULT_BACKEND = 'redis://localhost:6380/0'
 CELERY_REDIS_DB = 0
